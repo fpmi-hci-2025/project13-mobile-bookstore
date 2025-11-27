0619759e4cc9760403ead9d0e1895cfbd36261d3
Andreyeva Sofiya <Sofiy17.07.04@gmail.com>
2025-11-27 18:03:34 +0300

    add app colors

---
+++ lib/shared/theme/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color background = Color(0xFFFFFAF4);
+  static const Color primary = Color(0xFFB16668);
+  static const Color surface = Color(0xFFFFFFFF);
+  static const Color onPrimary = Color(0xFF121212);
+  static const Color secondary = Color(0xFFA6A6A6);
+}
