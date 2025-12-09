6da262fea831d334c4d28f73915be6b6a3b826ab
Andreyeva Sofiya <Sofiy17.07.04@gmail.com>
2025-12-09 21:06:31 +0300

    add app icons

---
+++ lib/shared/constants/app_icons.dart
@@ -0,0 +1,19 @@
+class AppIcons {
+  static const String arrowBack = 'assets/icons/arrowBack.svg';
+  static const String passwordOutline = 'assets/icons/passwordOutline.svg';
+  static const String home = 'assets/icons/home.svg';
+  static const String homeFill = 'assets/icons/homeFill.svg';
+  static const String category = 'assets/icons/category.svg';
+  static const String categoryFill = 'assets/icons/categoryFill.svg';
+  static const String buy = 'assets/icons/buy.svg';
+  static const String buyFill = 'assets/icons/buyFill.svg';
+  static const String profile = 'assets/icons/profile.svg';
+  static const String profileFill = 'assets/icons/profileFill.svg';
+  static const String minus = 'assets/icons/minus.svg';
+  static const String plus = 'assets/icons/plus.svg';
+  static const String notification = 'assets/icons/notification.svg';
+  static const String loupe = 'assets/icons/loupe.svg';
+  static const String location = 'assets/icons/location.svg';
+  static const String close = 'assets/icons/close.svg';
+  static const String detail =  'assets/icons/detail.svg';
+}
