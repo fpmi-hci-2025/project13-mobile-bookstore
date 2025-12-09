8b6362f88e368fde0d8a82880ceffdf2b79184d5
Andreyeva Sofiya <Sofiy17.07.04@gmail.com>
2025-12-09 21:05:23 +0300

    add app sizes

---
+++ lib/shared/constants/app_sizes.dart
@@ -0,0 +1,120 @@
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AppSizes {
+  //size width
+  static double sizeW4 = 4.w;
+  static double sizeW8 = 8.w;
+  static double sizeW12 = 12.w;
+  static double sizeW16 = 16.w;
+  static double sizeW18 = 18.w;
+  static double sizeW20 = 20.w;
+  static double sizeW24 = 24.w;
+  static double sizeW30 = 30.w;
+  static double sizeW36 = 36.w;
+  static double sizeW40 = 40.w;
+  static double sizeW48 = 48.w;
+  static double sizeW50 = 50.w;
+  static double sizeW56 = 56.w;
+  static double sizeW58 = 58.w;
+  static double sizeW60 = 60.w;
+  static double sizeW70 = 70.w;
+  static double sizeW74 = 74.w;
+  static double sizeW78 = 78.w;
+  static double sizeW82 = 82.w;
+  static double sizeW88 = 88.w;
+  static double sizeW93 = 93.w;
+  static double sizeW103 = 103.w;
+  static double sizeW100 = 100.w;
+  static double sizeW107 = 107.w;
+  static double sizeW111 = 111.w;
+  static double sizeW119 = 119.w;
+    static double sizeW120 = 120.w;
+  static double sizeW163 = 163.w;
+  static double sizeW167 = 167.w;
+  static double sizeW253 = 253.w;
+  static double sizeW270 = 270.w;
+  static double sizeW375 = 375.w;
+
+  //size height
+  static double sizeH1 = 1.h;
+  static double sizeH4 = 4.h;
+  static double sizeH6 = 6.h;
+  static double sizeH8 = 8.h;
+  static double sizeH10 = 10.h;
+  static double sizeH12 = 12.h;
+  static double sizeH16 = 16.h;
+  static double sizeH18 = 18.h;
+  static double sizeH20 = 20.h;
+  static double sizeH24 = 24.h;
+  static double sizeH28 = 28.h;
+  static double sizeH32 = 32.h;
+  static double sizeH34 = 34.h;
+  static double sizeH40 = 40.h;
+  static double sizeH48 = 48.h;
+  static double size47H = 47.h;
+  static double sizeH56 = 56.h;
+  static double sizeH63 = 63.h;
+  static double sizeH70 = 70.h;
+  static double sizeH77 = 77.h;
+  static double sizeH88 = 88.h;
+  static double sizeH100 = 100.h;
+  static double sizeH120 = 120.h;
+  static double sizeH190 = 190.h;
+  static double sizeH240 = 240.h;
+
+  //border radius
+  static double borderSize4 = 4.r;
+  static double borderSize8 = 8.r;
+  static double borderSize10 = 10.r;
+  static double borderSize12 = 12.r;
+  static double borderSize16 = 16.r;
+  static double borderSize20 = 20.r;
+  static double borderSize24 = 24.r;
+  static double borderSize32 = 32.r;
+  static double borderSize34 = 34.r;
+
+  //const size
+  static const double constSize2 = 2;
+  static const double constSize3 = 3;
+  static const double constSize5 = 5;
+  static const double constSize4 = 4;
+  static const double constSize6 = 6;
+  static const double constSize7 = 7;
+  static const double constSize9 = 9;
+   static const double constSize10 = 10;
+  static const double constSize12 = 12;
+  static const double constSize16 = 16;
+  static const double constSize20 = 20;
+  static const double constSize28 = 28;
+  static const double constSize24 = 24;
+  static const double constSize40 = 40;
+  static const double constSize32 = 32;
+  static const double constSize34 = 34;
+  static const double constSize36 = 36;
+  static const double constSize38 = 38;
+  static const double constSize44 = 44;
+  static const double constSize46 = 46;
+  static const double constSize56 = 56;
+  static const double constSize60 = 60;
+  static const double constSize68 = 68;
+  static const double constSize80 = 80;
+  static const double constSize82 = 82;
+  static const double constSize95 = 95;
+  static const double constSize98 = 98;
+  static const double constSize110 = 110;
+  static const double constSize114 = 114;
+  static const double constSize117 = 117;
+  static const double constSize119 = 119;
+  static const double constSize128 = 128;
+  static const double constSize134 = 134;
+  static const double constSize150 = 150;
+    static const double constSize140 = 140;
+  static const double constSize166 = 166;
+    static const double constSize180 = 180;
+  static const double constSize256 = 256;
+    static const double constSize240 = 240;
+  static const double constSize277 = 277;
+
+  //static height
+  static const double getStartedContainer = 400;
+}
